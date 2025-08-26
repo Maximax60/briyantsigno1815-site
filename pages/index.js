1790b28e46e766c15a66ebc7f080291264a8433a
bssigno1815 <briyantsigno@gmail.com>
2025-08-26 06:54:36 -0400

    Create pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{fontFamily:"system-ui,-apple-system,Segoe UI,Roboto",padding:"40px"}}>
+      <h1>BSS 1815 — Lakay (Next.js)</h1>
+      <p>Home page la ap mache. 404 pa dwe parèt ankò.</p>
+    </main>
+  );
+}
